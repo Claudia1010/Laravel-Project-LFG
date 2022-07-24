1c523e9b71feea16f37bed7cd05db2919ac834b7
Claudia1010 <claudiachristensen1@gmail.com>
2022-07-24 16:32:41 +0200

    feat: added channels table

---
+++ database/migrations/2022_07_24_141821_create_channels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->id();
+            $table->string('channel_name');
+            $table->unsignedBigInteger('game_id');
+            $table->timestamps();
+
+            $table->foreign('game_id')->references('id')->on('games')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+};
