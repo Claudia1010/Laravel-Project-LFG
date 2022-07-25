941b389f0f432315030c9a15869ebe51961b6235
Claudia1010 <claudiachristensen1@gmail.com>
2022-07-25 17:08:50 +0200

    Updated authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,13 +13,14 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
+  
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
         ]);
 
-        
+
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
