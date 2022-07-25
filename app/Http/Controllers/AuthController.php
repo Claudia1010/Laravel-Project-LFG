4225f3ab3d5e027e30d63d67c83b985dec9683a0
Claudia1010 <claudiachristensen1@gmail.com>
2022-07-25 16:38:02 +0200

    added register route

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,6 +19,7 @@ class AuthController extends Controller
             'password' => 'required|string|min:6',
         ]);
 
+        
         if ($validator->fails()) {
             return response()->json($validator->errors()->toJson(), 400);
         }
