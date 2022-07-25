c3da1574318958e8c13d24a2e0e4e20f38935705
Claudia1010 <claudiachristensen1@gmail.com>
2022-07-25 18:16:27 +0200

    trying to solve a bug

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,26 +13,28 @@ class AuthController extends Controller
 
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all(), [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors()->toJson(), 400);
-        }
-
-        $user = User::create([
-            'name' => $request->get('name'),
-            'email' => $request->get('email'),
-            'password' => bcrypt($request->password)
-        ]);
-
-        $user->roles()->attach(self::ROLE_USER);
-
-        $token = JWTAuth::fromUser($user);
-
-        return response()->json(compact('user', 'token'), 201);
+        return "hola";
     }
+    //     $validator = Validator::make($request->all(), [
+    //         'name' => 'required|string|max:255',
+    //         'email' => 'required|string|email|max:255|unique:users',
+    //         'password' => 'required|string|min:6',
+    //     ]);
+
+    //     if ($validator->fails()) {
+    //         return response()->json($validator->errors()->toJson(), 400);
+    //     }
+
+    //     $user = User::create([
+    //         'name' => $request->get('name'),
+    //         'email' => $request->get('email'),
+    //         'password' => bcrypt($request->password)
+    //     ]);
+
+    //     $user->roles()->attach(self::ROLE_USER);
+
+    //     $token = JWTAuth::fromUser($user);
+
+    //     return response()->json(compact('user', 'token'), 201);
+    // }
 }
--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/login', [AuthController::class, 'login']);
  
