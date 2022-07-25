e83e527882ca292948bf44dc48c6e49be2da72c0
Claudia1010 <claudiachristensen1@gmail.com>
2022-07-25 15:20:04 +0200

    feat: added AuthController and register function

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class AuthController extends Controller
+{
+    const ROLE_USER = 1;
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(), 400);
+        }
+
+        $user = User::create([
+            'name' => $request->get('name'),
+            'email' => $request->get('email'),
+            'password' => bcrypt($request->password)
+        ]);
+
+        $user->roles()->attach(self::ROLE_USER);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user', 'token'), 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
+Route::post('/register', [AuthController::class, 'register']);
