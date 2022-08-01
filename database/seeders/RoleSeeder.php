0f3492012515593fdbcb4967f2c8955d16f6346a
Claudia1010 <claudiachristensen1@gmail.com>
2022-08-02 00:29:24 +0200

    feat: added roleSeeder

---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('roles')->insert([
+            'id' => 1,
+            'name' => 'user',
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 2,
+            'name' => 'admin',
+        ]);
+
+        DB::table('roles')->insert([
+            'id' => 3,
+            'name' => 'superadmin',
+        ]);
+    }
+}
