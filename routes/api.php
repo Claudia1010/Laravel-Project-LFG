b7ff2bc31a288dabb3221c6532c7a6ed20930581
Claudia1010 <claudiachristensen1@gmail.com>
2022-08-01 11:51:53 +0200

    changed .env

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {return ['Bienvenido a mi api'];});
 
 //no token required
 Route::post('/register', [AuthController::class, 'register']);
